09a00751920caf2114549086b9516def4f08c582
mesfen <mesfen>
2022-04-07 12:47:49 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"programming is like 
+ /**
+    * main - Entry point
+     *
+      * Return: Always 0 (Success)
+       */
+ int main(void)
+	 {
+		           puts("\"programming is like building a multilingual puzzle");
+			             return (0);
+	 }
