713465a79a0a93f64e447f827b5f72a2986316d2
mesfen <mesfen145mogess@gmail.com>
2022-04-08 08:46:47 -0900

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ for (number1 = 0; number1 < 9; number1++)
 {
 for ( number2 = number1 + 1;  number2 < 10;  number2++)
 putchar((number1 % 10) + '0');
-putchar(( number2 % 10) + '0');
+putchar((number2 % 10) + '0');
 	
 if (number1 == 8 &&  number2 == 9)
 {
