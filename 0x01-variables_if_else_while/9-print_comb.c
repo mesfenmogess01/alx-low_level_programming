e286f11beb5f78ac660879db642d13973afa08f1
mesfen <mesfen145mogess@gmail.com>
2022-04-08 07:35:06 -0900

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int number;
+for (number = 48; number <= 57; number++)
+{
+putchar(number);
+if (number == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
+
