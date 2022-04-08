4e8dc1a036c2bea9b97ccf828e693a104c4503c2
mesfen <mesfen145mogess@gmail.com>
2022-04-08 06:09:40 -0900

    print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,15 +9,15 @@
 */
 int main(void)
 {
-	char alphabet;
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	{
-		if (alphabet == 'q')
-			continue;
-		else if (alphabet == 'e')
-			continue;
-		putchar(alphabet);
-	}
-	putchar('\n');
-	return (0);
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q')
+continue;
+else if (alphabet == 'e')
+continue;
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
 }
