2602a646ac7135a484842f15e4fa6462b7260d9b
mesfen <mesfen145mogess@gmail.com>
2022-04-08 07:00:09 -0900

    print_tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,13 +4,13 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 char alphabet;
-for (alphabet = 'z'; alphabet <= 'a'; alphabet++)
+for (alphabet = 'z'; alphabet <= 'a'; alphabet--)
 {
 putchar(alphabet);
 }
