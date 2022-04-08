a1603968d19c4fb3f10b3b7ad9137c55a63e0fb3
mesfen <mesfen145mogess@gmail.com>
2022-04-08 06:50:31 -0900

    5 and 6 task

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,16 +2,16 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
 */
 int main(void)
 {
-	int n;
-	for (n = 48; n <= 57; n++)
-	{
-		putchar(n);
-	}
-	putchar('\n');
-	return (0);
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
 }
