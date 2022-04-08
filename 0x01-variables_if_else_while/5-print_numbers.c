338a29cf3b25b91af371641d09e7bd62e3939365
mesfen <mesfen145mogess@gmail.com>
2022-04-08 06:19:31 -0900

    print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,11 +9,11 @@
 */
 int main(void)
 {
-	int number;
-	for (number = 0; number < 10; number++
-	{
-		printf("$number", number);
-	}
-	printf('\n');
-	return (0);
+int number;
+for (number = 0; number < 10; number++)
+{
+printf("$number", number);
+}
+printf('\n');
+return (0);
 }
