5d772e769a8dc35221754f0fbfc2aef9148fab3f
mesfen <mesfen145mogess@gmail.com>
2022-04-08 05:37:30 -0900

    lower_case_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,16 +2,16 @@
 #include <unistd.h>
 
 /**
- * *  * main - Entry point
- * *   *
- * *    * Return: Always 0 (Success)
- * */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char lower_case_alphabet;
-	for (lower_case_alphabet = 'a'; lower_case_alphabet <= 'z'; lower_case_alphabet++)
+	char lowerAlphabet;
+	for (lowerAlphabet = 'a'; lowerAlphabet <= 'z'; lowerAlphabet++)
 	{
-		putchar(lower_case_alphabet);
+		putchar(lowerAlphabet);
 	}
 	putchar('\n');
 	return (0);
