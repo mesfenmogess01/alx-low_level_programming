c28634389bcf095766f033c03ff24797e774717b
mesfen <mesfen145mogess@gmail.com>
2022-04-12 04:20:21 -0900

    absolut value of integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,6 +8,20 @@
 */
 int _abs(int n)
 {
-_putchar(abs(n));	
-return (0);
+
+	if (n < 0)
+	{
+		return (n * (-1));
+	}
+
+	else if (n == 0)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (n);
+	}
+
 }
