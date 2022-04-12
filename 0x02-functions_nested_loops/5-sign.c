a8cf1551ae11eaebbf87d36aef8f168f6b26eacf
mesfen <mesfen145mogess@gmail.com>
2022-04-12 03:37:37 -0900

    print the sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,18 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-	if (n == 0)
-	{
-		printf("0");
-		return (0);
-	}
-	else
-	{
-		printf("-");
-		return(-1);
-	}
-
-printf("+");
-return (1);
+	_putchar("+");
+	return (1);
+}
+else if(n == 0)
+{
+	printf("0");
+	return (0);
+}
+else
+{
+	printf("-");
+	return(-1);
 }
 return (0);
 }
