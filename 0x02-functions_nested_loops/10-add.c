a933e435cfffc43ca5b957f8cf2e9cd43771ae1f
mesfen <mesfen145mogess@gmail.com>
2022-04-12 10:35:18 -0900

    add two integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - add two integer
+ *
+ * Return: alweys 0
+*/
+int add(int a, int b)
+{
+	int n;
+	n = a + b;
+	_putchar("%d\n", n);
+	return (0);
+}
