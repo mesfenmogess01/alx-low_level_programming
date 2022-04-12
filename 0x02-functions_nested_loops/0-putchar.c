9d086bc9ba270882e69aa75028ceb41c7ff677fa
mesfen <mesfen145mogess@gmail.com>
2022-04-12 02:58:23 -0900

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,12 @@
 */
 int main(void)
 {
-	char putchar[] = "_putchar";
-	int c;
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(putchar[c]);
-	}
-	_putchar('\n');
-	return (0);
+char putchar[] = "_putchar";
+int c;
+for (c = 0; c < 8; c++)
+{
+	_putchar(putchar[c]);
+}
+_putchar('\n');
+return (0);
 }
