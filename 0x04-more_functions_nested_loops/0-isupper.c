039d8751cd7b87d41b7032a75d2c1bacee524795
mesfen <mesfen145mogess@gmail.com>
2022-04-14 11:21:48 -0900

    is upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _isupper - check if c is uppercase.
- *@c: print int
+ * @c: print int
  *
  * Return: Always 0.
  */
 
-int _isupper(int c);
+int _isupper(int c)
 {
 if (c >= 45 && c <= 90)
 {
