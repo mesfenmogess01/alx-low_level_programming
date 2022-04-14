727d3331c23c54580ea562d5271017ee246e5e8a
mesfen <mesfen145mogess@gmail.com>
2022-04-13 22:32:57 -0900

    chack is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c);
+{
+char = c;
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
