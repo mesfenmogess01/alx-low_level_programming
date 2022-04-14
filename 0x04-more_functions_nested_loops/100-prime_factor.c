377f1d00f80acd8c98a9a9f23b317ed22c0c0e4c
mesfen <mesfen145mogess@gmail.com>
2022-04-14 12:22:13 -0900

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print facter of number
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+unsigned long n, i, max;
+n = 6128524751443;
+max = 1;
+
+for (i = 3; i <= n; i +=2)
+{
+while (n % i == 0)
+{
+max = n;
+n = n / i;
+}
+}
+printf("%lu\n", max);
+return (0);
+}
