70112ab9f97079cb80ed2a7ac5dad0121716fb38
mesfen <mesfen145mogess@gmail.com>
2022-04-18 03:06:34 -0900

    swaps two numbers thos are integer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap.c - swaps two integer
+ * @c: swap number
+ *
+ * Return: Alweys 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	_putchar("a=%d, b=%d\n", a, b);
+	a = c;
+	a = b;
+	c = b;
+	_putchar("a=%d, b=%d\n", a, b);
+return (0);
+}
