0ac510b9b3557962ac88c2d21a7a99453cc0ab8a
mesfen <mesfen145mogess@gmail.com>
2022-04-25 07:14:13 -0900

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * simple_print_buffer - prints s in hexa
+ * @s: the address of memory to print
+ * @n: the size of the memory to print
+ *
+ * Return: s[i].
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsgined int i;
+i = 0;
+while ( i < size)
+{
+if (i % 10)
+{
+_puycahar(" ")
+}
+if (!(i % 10) && i)
+{
+_putchar("\n");
+}
+_putchar(s[i]);
+i++;
+}
+_putchar("\n");
+}
