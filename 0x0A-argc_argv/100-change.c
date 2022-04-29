2dea3516553688a23333aafb78ed1455d69d88e2
mesfen <mesfen145mogess@gmail.com>
2022-04-29 10:19:57 -0900

    changs

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,9 +5,9 @@
 #include <stdbool.h>
 
 /**
- * coinConverter - Helper function that does all the mathematics
+ * coinConverter - Helper function
  * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
+ * Return: The number of coins
  */
 int coinConverter(int i)
 {
@@ -40,10 +40,10 @@ int coinConverter(int i)
 }
 
 /**
- * main - Takes in exactly one argument for minimum coin count
+ * main - Takes in exactly one argument
  * @argc: Number of command line arguments
  * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ * Return: 0 if exactly 1 argument
  */
 int main(int argc, char *argv[])
 {
