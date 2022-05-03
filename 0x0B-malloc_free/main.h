f6895268ee0797dc09edad54c59afcf8fde8ba8c
mesfen <mesfen145mogess@gmail.com>
2022-05-03 01:42:02 -0900

    strdup

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_HEADER
-#define _HOLBERTON_HEADER
+#ifndef _MAIN_H
+#define _MAIN_H
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
