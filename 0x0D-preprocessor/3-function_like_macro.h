ccae8857ceca965991f83606fcd754b18243faaa
mesfen <mesfen145mogess@gmail.com>
2022-05-09 08:24:24 -0900

    function like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,5 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-
-/*
- * File: 3-function_like_macro.h
- * Desc: Header file function-like macro ABS(x)
- */
-
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
