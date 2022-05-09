658dd30ecdb4f80f5ac49febfc769d04773601e4
mesfen <mesfen145mogess@gmail.com>
2022-05-09 08:21:53 -0900

    sum

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -4,8 +4,8 @@
 /*
  * File: 3-function_like_macro.h
  * Desc: Header file function-like macro ABS(x)
- * that computes the absolute value of a number x.
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
+
 #endif
