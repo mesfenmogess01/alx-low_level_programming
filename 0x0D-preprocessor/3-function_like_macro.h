d262ffefe1d66e9cbc486873e8f40623e9609b61
mesfen <mesfen145mogess@gmail.com>
2022-05-09 08:17:59 -0900

    function like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,10 +3,9 @@
 
 /*
  * File: 3-function_like_macro.h
- * Desc: Header file that defines a function-like macro ABS(x)
+ * Desc: Header file function-like macro ABS(x)
  * that computes the absolute value of a number x.
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
-
 #endif
