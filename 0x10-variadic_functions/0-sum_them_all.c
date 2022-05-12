223dd01fb3f9fcd4e0dce721db6d6fafa8b10f8a
mesfen <mesfen145mogess@gmail.com>
2022-05-12 13:06:06 -0900

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,6 +2,7 @@
 /**
  * sum_them_all - returns the sum of all parameters
  * @n: the numbers of arguments being passed through
+ * @...: countins
  * Return: the sum of all parameters
  */
 int sum_them_all(const unsigned int n, ...)
@@ -10,18 +11,14 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int sum = 0, i;
 
 	if (n == 0)
-	{
 		return (0);
 
 	va_start(valist, n);
-	}
 
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(valist, int);
 
 	va_end(valist);
-	}
 
 	return (sum);
 }
