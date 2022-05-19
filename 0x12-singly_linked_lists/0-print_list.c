cdf21988fe38a93683a12c8fc0be820aec2f6027
mesfen <mesfen145mogess@gmail.com>
2022-05-19 04:40:18 -0900

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t l = 0;
+	size_t list = 0;
 
 	while (h)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
+		list++;
 		h = h->next;
-		l++;
 	}
 
-	return (l);
+	return (list);
 }
