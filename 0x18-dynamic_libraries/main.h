6aa6128cb1c3e5bc651cb98a2ef6554c958bd365
mesfen <mesfen145mogess@gmail.com>
2022-07-14 19:26:59 +0300

    main

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -27,7 +24,7 @@ char *_strstr(char *haystack, char *needle);
 int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
+int div(int a, int b);
 int mod(int a, int b);
-extern div_t div(int __numer, int __denom);
 
-#endif /* MAIN_H */
+#endif
