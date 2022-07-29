bd1cf76aa0014472fdf3c4c9ef6c248dd54ba08f
mesfen <mesfen145mogess@gmail.com>
2022-07-29 11:35:57 +0300

    hash tabel creat

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table of size @size
+ * hash_table_create - creates a hash table (as name implies) of size @size
  *
- * @size: The size of the hash table
+ * @size: The size of the hash table (the spaces allocated for nodes)
  * Return: pointer to the hash table on success, else, null
  */
 hash_table_t *hash_table_create(unsigned long int size)
