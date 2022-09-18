3da879d898e1edd026c308a8d4041449bc7d0069
mesfen <mesfen145mogess@gmail.com>
2022-09-18 08:32:35 +0300

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Module that calculates the perimeter of an island in a grid."""
-
+"""Module that calculates the perimeter of an island in a grid.
+"""
 
 def num_water_neighbors(grid, i, j):
     """Returns the number of water neighbors a cell has in a grid."""
@@ -9,14 +9,13 @@ def num_water_neighbors(grid, i, j):
 
     if i <= 0 or not grid[i - 1][j]:
         num += 1
-        if j <= 0 or not grid[i][j - 1]:
-            num += 1
-        if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
-            num += 1
-        if i >= len(grid) - 1 or not grid[i + 1][j]:
-            num += 1
-
-        return num
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+    return num
 
 
 def island_perimeter(grid):
