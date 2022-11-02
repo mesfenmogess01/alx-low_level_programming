7f1796624577e658a45e340cf12fe301d898de70
mesfen <mesfen145mogess@gmail.com>
2022-11-02 11:35:00 +0300

    liner algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,6 +7,7 @@
  * @value: The value to search for.
  *
  * Return: If the value is not present or the array is NULL, -1.
+ *        Otherwise, the first index where the value is located.
  *
  * Description: Prints a value every time it is compared in the array.
  */
@@ -24,5 +25,5 @@ int linear_search(int *array, size_t size, int value)
 			return (i);
 	}
 
-	return (-1);
+return (-1);
 }
